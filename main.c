9aff1016563918fc0fe174decd2c56d8bfe987d0
wboateng72 <boatengwilliam72@gmail.com>
2023-08-23 00:09:21 +0000

    main.c reviewed

--- main.c
+++ main.c
@@ -4,41 +4,39 @@
 
 /**
  * main - Entry point
- *
+ * William and Sheriff
  * Return: Always 0
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+int length;
+int length2;
+unsigned int ui;
+void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
-    return (0);
+ui = (unsigned int)INT_MAX + 1024;
+length = _printf("Percent:[%%]\n");
+length2 = printf("Percent:[%%]\n");
+_printf("Length:[%d]\n", length);
+printf("Length:[%d]\n", length2);
+_printf("Unsigned:[%u]\n", ui);
+printf("Unsigned:[%u]\n", ui);
+length = _printf("Let's try to printf a simple sentence.\n");
+length2 = printf("Let's try to printf a simple sentence.\n");
+addr = (void *)0x7ffe637541f0;
+_printf("Length:[%d, %i]\n", length, length);
+printf("Length:[%d, %i]\n", length2, length2);
+_printf("Negative:[%d]\n", -762534);
+printf("Negative:[%d]\n", -762534);
+_printf("Unsigned octal:[%o]\n", ui);
+printf("Unsigned octal:[%o]\n", ui);
+_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+_printf("Character:[%c]\n", 'T');
+printf("Character:[%c]\n", 'T');
+_printf("String:[%s]\n", "Our first project !");
+printf("String:[%s]\n", "Our first project !");
+_printf("Address:[%p]\n", addr);
+printf("Address:[%p]\n", addr);
+return (0);
 }
